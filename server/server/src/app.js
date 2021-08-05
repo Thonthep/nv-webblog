25591e0fa59981c30a5b62c733a5f02b5f4a27ed
Thonthep <Thontheph62@nu.ac.th>
2021-08-06 03:37:15 +0700

    new

--- server/server/src/app.js
+++ server/server/src/app.js
@@ -1,12 +1,12 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 const {sequelize} = require('./models')
-
+let cors = require('cors')
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(cors())
 require('./routes')(app)
 
 app.get('/status', function (req, res ){
